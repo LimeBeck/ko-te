70c92e240b111810231c7b63fba2990475f23c81
LimeBeck <becklime@gmail.com>
2021-12-10 08:56:28 +0300

    * Start implement conditional block parser

--- src/commonMain/kotlin/parser/ast/ConditionalBlockParser.kt
+++ src/commonMain/kotlin/parser/ast/ConditionalBlockParser.kt
@@ -1,8 +1,10 @@
 package dev.limebeck.templateEngine.parser.ast
 
 import dev.limebeck.templateEngine.inputStream.RewindableInputStream
+import dev.limebeck.templateEngine.inputStream.readUntil
 import dev.limebeck.templateEngine.parser.LanguageToken
 import dev.limebeck.templateEngine.inputStream.skipNext
+import dev.limebeck.templateEngine.inputStream.toStream
 
 object ConditionalBlockParser : AstLexemeParser<AstLexeme> {
     override fun canParse(stream: RewindableInputStream<LanguageToken>): Boolean {
@@ -14,8 +16,27 @@ object ConditionalBlockParser : AstLexemeParser<AstLexeme> {
     override fun parse(stream: RewindableInputStream<LanguageToken>): AstLexeme {
         val nextToken = stream.peek()
         if(!canParse(stream))
-            throw stream.throwErrorOnValue("conditional block")
+            stream.throwErrorOnValue("conditional block")
+        if(!(nextToken is LanguageToken.Keyword && nextToken.name == "if"))
+            stream.throwErrorOnValue("'if' keyword")
         stream.skipNext(1)
+
+        val possibleBracket = stream.peek()
+        val hasOpenBracket = possibleBracket is LanguageToken.Punctuation && possibleBracket.value == "("
+        if (!hasOpenBracket)
+            stream.throwErrorOnValue("punctuation '('")
+        stream.skipNext(1)
+
+        val innerValues = stream.readUntil { !(it is LanguageToken.Punctuation && it.value == ")") }
+        val closedBracket = stream.peek()
+        val hasClosedBracket = closedBracket is LanguageToken.Punctuation && closedBracket.value == ")"
+        if (!hasClosedBracket)
+            stream.throwErrorOnValue("punctuation ')'")
+
+        val condition = ValueParser.parse(innerValues.toStream())
+
+
+        // return AstLexeme.Conditional(condition = condition, then = )
         TODO("ConditionalBlockParser")
     }
 }
